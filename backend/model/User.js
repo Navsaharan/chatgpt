20f11f3eccd960f40d305620105fc4a94cc40fb4
Navsaharan <navjeetsaharan8@gmail.com>
2025-02-18 15:45:28 +0530

    Create User.js

---
+++ backend/model/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    brokerAPI: {
+        zerodha: String,
+        upstox: String,
+        angel: String
+    }
+});
+
+module.exports = mongoose.model("User", UserSchema);
