f133e145551b61881ce3ca148f0ca31202071fde
Navsaharan <navjeetsaharan8@gmail.com>
2025-02-18 16:26:51 +0530

    Update stockRoutes.js

--- backend/routes/stockRoutes.js
+++ backend/routes/stockRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const { getStockPrice } = require("../controllers/stockController");
 
 router.get("/price/:stockSymbol", getStockPrice);
+const cacheMiddleware = require("../middleware/cacheMiddleware");
+
+router.get("/price/:stockSymbol", cacheMiddleware, getStockPrice);
 
 module.exports = router;
